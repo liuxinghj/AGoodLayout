6d2a1bae99d6ea7c1760bf11f87f189853b04aa8
alion <171255311@qq.com>
2016-07-23 15:39:34 +0800

    [code] flowlayout realize

--- library/src/main/java/com/alion/library/FlowLayout.java
+++ library/src/main/java/com/alion/library/FlowLayout.java
@@ -185,7 +185,6 @@ public class FlowLayout
                               ? childHeight
                               : mLineHeight;
             }
-
             //save child
             mViews.add(view);
 
